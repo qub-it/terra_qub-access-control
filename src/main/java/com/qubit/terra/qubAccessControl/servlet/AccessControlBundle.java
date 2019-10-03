e2fbb8bda0b233761b28f023cea4edb07a8da1c1
Daniel Pires <daniel.pires@qub-it.com>
2019-10-03 13:12:45 +0000

    added method to get localized string of bundle

--- src/main/java/com/qubit/terra/qubAccessControl/servlet/AccessControlBundle.java
+++ src/main/java/com/qubit/terra/qubAccessControl/servlet/AccessControlBundle.java
@@ -3,22 +3,34 @@ package com.qubit.terra.qubAccessControl.servlet;
 import java.util.ResourceBundle;
 import java.util.regex.Matcher;
 
+import org.apache.commons.lang.LocaleUtils;
+
 public class AccessControlBundle {
 
 	private static final String BUNDLE_NAME = "resources.AccesscontrolResources";
 	private static ResourceBundle bundle = ResourceBundle.getBundle(BUNDLE_NAME);
 
-	public static String accessControlBundle(String key) {
+	public static String get(String key) {
 		return bundle.getString(key);
 	}
 
-	public static String accessControlBundle(String key, String... args) {
+	public static String get(String key, String... args) {
 
 		String message = bundle.getString(key);
 		for (int i = 0; i < args.length; i++) {
 			message = message.replaceAll("\\{" + i + "\\}", args[i] == null ? "" : Matcher.quoteReplacement(args[i]));
 		}
 		return message;
+
+	}
+
+	public static String localizedString(String key) {
+
+		String keyPT = ResourceBundle.getBundle(BUNDLE_NAME, LocaleUtils.toLocale("pt")).getString(key);
+
+		String keyEN = ResourceBundle.getBundle(BUNDLE_NAME, LocaleUtils.toLocale("en")).getString(key);
+
+		return "{\"pt_PT\":\"" + keyPT + "\",\"en_GB\":\"" + keyEN + "\"}";
 
 	}
 
