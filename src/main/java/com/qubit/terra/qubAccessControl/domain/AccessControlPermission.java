44be367c08c221ceb9b772580495f4ed048203a4
Daniel Pires <daniel.pires@qub-it.com>
2020-02-19 17:05:18 +0000

    fixed permission cache bug

--- src/main/java/com/qubit/terra/qubAccessControl/domain/AccessControlPermission.java
+++ src/main/java/com/qubit/terra/qubAccessControl/domain/AccessControlPermission.java
@@ -17,85 +17,88 @@ public class AccessControlPermission extends AccessControlPermission_Base {
     static final private Cache<String, Optional<AccessControlPermission>> CACHE =
             CacheBuilder.newBuilder().concurrencyLevel(4).maximumSize(10 * 1000).expireAfterWrite(2, TimeUnit.HOURS).build();
 
-	protected AccessControlPermission() {
-		super();
-		setDomainRoot(pt.ist.fenixframework.FenixFramework.getDomainRoot());
-	}
+    protected AccessControlPermission() {
+        super();
+        setDomainRoot(pt.ist.fenixframework.FenixFramework.getDomainRoot());
+    }
 
-	protected AccessControlPermission(String rawName, Boolean restricted, String code, Boolean locked) {
-		this();
-		setRawName(rawName);
-		setRestricted(restricted);
-		setCode(code);
-		setLocked(locked);
-		checkRules();
+    protected AccessControlPermission(String rawName, Boolean restricted, String code, Boolean locked) {
+        this();
+        setRawName(rawName);
+        setRestricted(restricted);
+        setCode(code);
+        setLocked(locked);
+        checkRules();
         CACHE.put(code, Optional.of(this));
-	}
+    }
 
-	private void checkRules() {
-		if (getDomainRoot() == null) {
-			throw new IllegalStateException(AccessControlBundle.get("error.domainRoot.required"));
-		}
+    private void checkRules() {
+        if (getDomainRoot() == null) {
+            throw new IllegalStateException(AccessControlBundle.get("error.domainRoot.required"));
+        }
 
-		if (getRawName() == null) {
-			throw new IllegalStateException(AccessControlBundle.get("error.AccessControlPermission.rawName.required"));
-		}
-		if (getRestricted() == null) {
-			throw new IllegalStateException(
-					AccessControlBundle.get("error.AccessControlPermission.restricted.required"));
-		}
-		if (getCode() == null) {
-			throw new IllegalStateException(AccessControlBundle.get("error.AccessControlPermission.code.required"));
-		}
-		if (getLocked() == null) {
-			throw new IllegalStateException(
-					AccessControlBundle.get("error.AccessControlPermission.restricted.required"));
-		}
-	}
+        if (getRawName() == null) {
+            throw new IllegalStateException(AccessControlBundle.get("error.AccessControlPermission.rawName.required"));
+        }
+        if (getRestricted() == null) {
+            throw new IllegalStateException(AccessControlBundle.get("error.AccessControlPermission.restricted.required"));
+        }
+        if (getCode() == null) {
+            throw new IllegalStateException(AccessControlBundle.get("error.AccessControlPermission.code.required"));
+        }
+        if (getLocked() == null) {
+            throw new IllegalStateException(AccessControlBundle.get("error.AccessControlPermission.restricted.required"));
+        }
+    }
 
-	public static AccessControlPermission create(String rawName, Boolean restricted, String code, Boolean locked) {
-		if (findByCode(code) == null) {
-			return new AccessControlPermission(rawName, restricted, code, locked);
-		} else {
-			throw new IllegalArgumentException(
-					AccessControlBundle.get("error.AccessControlPermission.code.exists", code));
-		}
+    public static AccessControlPermission create(String rawName, Boolean restricted, String code, Boolean locked) {
+        if (findByCode(code) == null) {
+            return new AccessControlPermission(rawName, restricted, code, locked);
+        } else {
+            throw new IllegalArgumentException(AccessControlBundle.get("error.AccessControlPermission.code.exists", code));
+        }
 
-	}
+    }
 
-	public static AccessControlPermission findByCode(String code) {
+    public static AccessControlPermission findByCode(String code) {
         try {
-            return CACHE.get(code, () -> findAll().stream().filter(op -> op.getCode().equals(code)).findFirst()).orElse(null);
+            AccessControlPermission result =
+                    CACHE.get(code, () -> findAll().stream().filter(op -> op.getCode().equals(code)).findFirst()).orElse(null);
+            if (result != null && FenixFramework.isDomainObjectValid(result)) {
+                return result;
+            }
+            CACHE.invalidate(code);
+            return null;
         } catch (ExecutionException e) {
             return null;
         }
-	}
+    }
 
-	public static Set<AccessControlPermission> findAll() {
-		return FenixFramework.getDomainRoot().getPermissionsSet();
-	}
+    public static Set<AccessControlPermission> findAll() {
+        return FenixFramework.getDomainRoot().getPermissionsSet();
+    }
 
-	@pt.ist.fenixframework.Atomic
-	public void delete() {
-		if (!getProfileSet().isEmpty()) {
-			throw new IllegalStateException(AccessControlBundle.get("error.AccessControlPermission.delete")
-					+ getProfileSet().stream().map(profile -> profile.getRawName()).collect(Collectors.joining(",")));
-		}
+    @pt.ist.fenixframework.Atomic
+    public void delete() {
+        if (!getProfileSet().isEmpty()) {
+            throw new IllegalStateException(AccessControlBundle.get("error.AccessControlPermission.delete")
+                    + getProfileSet().stream().map(profile -> profile.getRawName()).collect(Collectors.joining(",")));
+        }
 
-		setDomainRoot(null);
-		super.deleteDomainObject();
+        setDomainRoot(null);
+        super.deleteDomainObject();
         CACHE.invalidate(getCode());
-	}
+    }
 
-	public String getExpression() {
-		return "permission(" + getCode() + ")";
-	}
+    public String getExpression() {
+        return "permission(" + getCode() + ")";
+    }
 
-	public Boolean isRestricted() {
-		return getRestricted();
-	}
+    public Boolean isRestricted() {
+        return getRestricted();
+    }
 
-	public Boolean isLocked() {
-		return getLocked();
-	}
+    public Boolean isLocked() {
+        return getLocked();
+    }
 }
