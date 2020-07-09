cacc2ed058f6ed52a76c64bef7c5081d854a34ce
Daniel Pires <daniel.pires@qub-it.com>
2020-07-09 15:05:24 +0000

    refactored providers map

--- src/main/java/com/qubit/terra/qubAccessControl/domain/ProviderStrategy.java
+++ src/main/java/com/qubit/terra/qubAccessControl/domain/ProviderStrategy.java
@@ -1,12 +1,15 @@
 package com.qubit.terra.qubAccessControl.domain;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public abstract class ProviderStrategy<T extends Object> {
+
+    protected static final Map<String, ProviderStrategy> PROVIDERS = new HashMap<String, ProviderStrategy>();
 
     private String name;
     private Function<AccessControlProfile, Set<T>> provideFunction;
@@ -31,6 +34,20 @@ public abstract class ProviderStrategy<T extends Object> {
         return this.name;
     }
 
-    public static final Map<String, ProviderStrategy> PROVIDERS = new HashMap<String, ProviderStrategy>();
+    public static void registerProvider(String name, ProviderStrategy provider) {
+        PROVIDERS.put(name, provider);
+    }
+
+    public static ProviderStrategy getProvider(String name) {
+        return PROVIDERS.get(name);
+    }
+
+    public static Set<String> getProvidersKeys() {
+        return PROVIDERS.keySet();
+    }
+
+    public static Set<ProviderStrategy> getProviders() {
+        return new HashSet<>(PROVIDERS.values());
+    }
 
 }
